6d04515348cbc4d308597a23c4cbcfcfc8d70ac2
Bryan Hadro <bryan.e.hadro@espn.com>
2013-10-17 16:29:08 -0400

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,72 @@
+function Utils() { }
+
+Utils.httpGet = function(url, callback) {
+	var callback = callback || function() {};
+
+	var oReq = new XMLHttpRequest();
+	oReq.onload = callback;
+	oReq.open("get", url, true);
+	oReq.send();
+	return;
+};
+
+Utils.getScript = function(js, callback) {
+	
+	var callback = callback || function() {};
+	
+	var s = document.createElement("script");
+	s.setAttribute("language","javascript");
+	s.setAttribute("type","text/javascript");
+	s.setAttribute("src", js);
+	s.onload = callback;
+	document.body.appendChild(s);
+	return;
+};
+
+Utils.getTime = function() {
+	return new Date().getTime();
+};
+
+Utils.log = function() {
+
+	var elem = null;
+
+	if (arguments.length > 0 && typeof arguments[0] === "string") {
+		
+		var selector = arguments[0];
+		
+		if (selector.indexOf("#") === 0) {
+			elem = document.getElementById( selector.substr(1, selector.length));
+		} else if (selector.indexOf(".") === 0) {
+			elem = document.getElementsByClassName(selector.substr(1, selector.length))[0];
+		}
+	}
+	
+	if (!!elem) {
+		var str = elem.innerHTML;
+		elem.innerHTML = Array.prototype.slice.call(arguments, 1) + "\n" + str;
+	} else {
+		try {
+			console.log.apply(console, arguments);
+		} catch (e) {
+			try {
+				opera.postError.apply(opera, arguments);
+			} catch(e) {
+				alert(Array.prototype.join.call( arguments, " "));
+			}
+		}
+	}
+};
+
+Utils.parseUri = function(key) {
+	var val = null,
+	search = window.location.search,
+	search = search.substr(1,search.length);
+	
+	var arr1 = search.split("&"), i = arr1.length, arr2;
+	while(i--) {
+		arr2 = arr1[i].split("=");
+		if (arr2[0] == key) { val = arr2[1]; break; }
+	}
+	return val;
+};
